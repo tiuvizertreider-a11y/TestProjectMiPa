f696242d3d070d96c5467d2cce5977d76f463bd2
Павел Муслаев <muslaevpavel@gmail.com>
2026-01-26 13:48:37 +0300

    Реализовал класс Student, со специальным методом __str__().

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+class Student:
+    def __init__(self, name: str, age: int, email: str | None=None):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'{self.name.title()} {self.age} years old'
